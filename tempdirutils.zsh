ba9292f0cc912aed008f6c74efc4f5597ba9bbd7
watarena <watarena>
2022-03-12 13:56:09 +0900

    refactor completion fuction

--- tempdirutils.zsh
+++ tempdirutils.zsh
@@ -1,9 +1,20 @@
 cdtemp() {
-    [ $# -eq 0 ] && cd $(mktemp -d) || cd ${TMPDIR}$1
+    [ $# -eq 0 ] && cd $(mktemp -d) || cd "${TMPDIR%/}/$1"
+}
+
+_list_tempdirs() {
+    local dir files
+    for dir in "${TMPDIR%/}"/tmp.*/; do
+        files=$(echo "${dir}"*(N))
+        tempdirs+=( "${dir##${TMPDIR%/}/}[${files//${dir}/}]" )
+    done
 }
 
 _cdtemp() {
-    eval _values 'tempdirs' $(ls -F ${TMPDIR} | grep 'tmp\..*/' | xargs -I{} sh -c 'echo "{}[$(ls ${TMPDIR}{} | xargs)]"' | sed "s/^.*$/'&'/" | tr '\n' ' ')
+    local tempdirs
+    tempdirs=()
+    _list_tempdirs
+    _values 'tempdirs' "${tempdirs[@]}"
 }
 
 compdef _cdtemp cdtemp
