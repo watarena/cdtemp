9e325a3f46c2205c9ea4f4fc2940fb4cad96dcf3
watarena <watarena>
2022-03-13 10:00:35 +0900

    add rmtemp

--- tempdirutils.zsh
+++ tempdirutils.zsh
@@ -2,6 +2,11 @@ cdtemp() {
     [ $# -eq 0 ] && cd $(mktemp -d) || cd "${TMPDIR%/}/$1"
 }
 
+rmtemp() (
+    cd "${TMPDIR}"
+    rm -rf "$@"
+)
+
 _list_tempdirs() {
     local dir files
     for dir in "${TMPDIR%/}"/tmp.*/; do
@@ -14,7 +19,27 @@ _cdtemp() {
     local tempdirs
     tempdirs=()
     _list_tempdirs
-    _values 'tempdirs' "${tempdirs[@]}"
+    if [ ${#tempdirs} -gt 0 ]; then
+        _values 'tempdirs' "${tempdirs[@]}"
+    else
+        _values 'tempdirs' ''
+    fi
+}
+
+_rmtemp() {
+    local tempdirs
+    tempdirs=()
+    _list_tempdirs
+    for arg in ${words:1}; do
+      tempdirs=( ${tempdirs:#${arg}\[*]} )
+    done
+    if [ ${#tempdirs} -gt 0 ]; then
+        _values 'tempdirs' "${tempdirs[@]}"
+    else
+        _values 'tempdirs' ''
+    fi
 }
 
 compdef _cdtemp cdtemp
+compdef _rmtemp rmtemp
+
